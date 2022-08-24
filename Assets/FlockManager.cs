b0457d1340893d846bbcc25b4665285877e64a99
Erick Lima <70773204+ErickLima13@users.noreply.github.com>
2022-08-24 10:41:16 -0300

    finish flock

--- Assets/FlockManager.cs
+++ Assets/FlockManager.cs
@@ -8,14 +8,13 @@ public class FlockManager : MonoBehaviour
     public int numFish = 20;
     public GameObject[] allFish;
     public Vector3 swimLimits = new(5, 5, 5);
+    public Vector3 goalPos;
 
     [Header("Fish Settings")]
     [Range(0.0f, 5.0f)] public float minSpeed;
     [Range(0.0f, 5.0f)] public float maxSpeed;
     [Range(1.0f, 10.0f)] public float neighbourDistance;
     [Range(0.0f, 5.0f)] public float rotationSpeed;
-   
-
 
     // Start is called before the first frame update
     void Start()
@@ -31,11 +30,20 @@ public class FlockManager : MonoBehaviour
             allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.identity);
             allFish[i].GetComponent<Flock>().myManager = this;
         }
+
+        goalPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Random.Range(0, 100) < 10)
+        {
+            goalPos = transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x),
+                                                Random.Range(-swimLimits.y, swimLimits.y),
+                                                Random.Range(-swimLimits.z, swimLimits.z));
+        }
+
+
     }
 }
